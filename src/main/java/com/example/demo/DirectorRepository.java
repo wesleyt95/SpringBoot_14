591eb0bca70cfd862813a7b750cd2e4b961399a5
Wesley Bond <wesley.bond@icloud.com>
2017-12-04 15:41:56 -0500

    asdsd

---
+++ src/main/java/com/example/demo/DirectorRepository.java
@@ -0,0 +1,5 @@
+package com.example.demo;
+
+public interface DirectorRepository
+        extends CrudRepository<Director, Long>{
+}
---
+++ src/main/java/com/example/demo/MovieRepository.java
@@ -0,0 +1,4 @@
+package com.example.demo;
+
+public interface MovieRepository extends CrudRepository<Movie, Long>{
+}
